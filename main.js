265417c1bb4d41c831dc87cfeed125853bd34a94
HarukaOtani <haruka_yamashita_aa@mail.toyota.co.jp>
2025-07-30 13:03:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,23 @@ const board = [
 ]
 const array = [10,120,230,340];
 
-function reset(){
+function addOne(num){
+  return num += 1;
+}
+
+function reset() {
+  let counter = 0;
   for (let x = 0; x < 4; x++){
     for(let y = 0; y < 4; y++){
+      if (board[y][x] !== 0){
+        counter = addOne(counter);
+      }
       board[y][x] = 0;
     }
   }
-  gamestart();
+  if (counter >= 1){
+    gamestart();
+  }
 }
 
 function gamestart(){
@@ -24,7 +34,7 @@ function gamestart(){
   for (let y = 1; y < 4; y++){
     for (let x = 0; x < 4; x++){
       if (board[y][x] !== 0){
-        counter += 1;
+        counter = addOne(counter);
       }
     }
   }
@@ -42,8 +52,8 @@ function addBoardNew2(){
   let flag = 0;
   
   do {
-    x2 = Math.floor(Math.random() * 4);//0~3
-    y2 = Math.floor(Math.random() * 4);//0~3
+    x2 = Math.floor(Math.random() * 4);
+    y2 = Math.floor(Math.random() * 4);
     if(board[y2][x2] === 0){
       board[y2][x2] = 2;
       flag = 1;
@@ -54,21 +64,41 @@ function addBoardNew2(){
 }
 
 function canvusReflesh(){
-  ctx.fillStyle = "#4de7ffff";
+  ctx.fillStyle = "rgb(213, 213, 213)"; 
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  for (let x = 0; x < 4; x++){
-    for(let y = 0; y < 4; y++){
-      if(board[y][x] > 0){
+  for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < 4; y++) {
+      if (board[y][x] > 0){
         const number = board[y][x];
-        ctx.fillStyle = "#ffffff";
+        if (number === 2){
+          ctx.fillStyle = "#ffe9bcff";
+        } else if (number === 4){
+          ctx.fillStyle = "#f1fcbcff";
+        } else if (number === 8){
+          ctx.fillStyle = "#d4ffbfff";
+        } else if (number === 16){
+          ctx.fillStyle = "#bfffc9ff";
+        } else if (number === 32){
+          ctx.fillStyle = "#c0ffeaff";
+        } else if (number === 64){
+          ctx.fillStyle = "#bff4ffff";
+        } else if (number === 128){
+          ctx.fillStyle = "#bcd2fdff";
+        } else if (number === 256){
+          ctx.fillStyle = "#cfc6ffff";
+        } else if (number === 512){
+          ctx.fillStyle = "#eac0ffff";
+        }  else if (number === 1024){
+          ctx.fillStyle = "#ffbdf4ff";
+        } else if (number === 2048){
+          ctx.fillStyle = "#ffc3d7ff";
+        }
         ctx.fillRect(array[x], array[y], 100, 100);
-
-        ctx.font = "40px Arial";
+        ctx.font = "40px Arial"; 
         ctx.fillStyle = "black";
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
-        ctx.fillText(number, array[x] + 50, array[y] + 50);
+        ctx.fillText (number, array[x] + 50, array[y] + 50);
       }
     }
   }
@@ -83,16 +113,28 @@ function upBlock(){
                     if (board[i][x] === 0){
                         board[i][x] = board[i + 1][x];
                         board[i + 1][x] = 0;
-                        counter += 1;
+                        counter = addOne(counter);
                     }else if (board[i][x] === board[i + 1][x]){
                         board[i][x] = board[i + 1][x] * 2;
                         board[i + 1][x] = 0;
-                        counter += 1; 
+                        counter = addOne(counter);
     }}}}}
     if(counter !== 0){
         addBoardNew2();
     }                 
 }
+
+document.body.addEventListener("keydown",(e) => {
+  if (e.key === "ArrowUp"){
+    upBlock();
+  } else if (e.key === "ArrowDown"){
+    downBlock();
+  } else if (e.key === "ArrowRight"){
+    rightBlock();
+  } else if (e.key === "ArrowLeft"){
+    leftBlock();
+  }
+});
 
 function downBlock(){
   let counter = 0;
@@ -103,11 +145,11 @@ function downBlock(){
             if (board[i][x] === 0){
               board[i][x] = board[i - 1][x];
               board[i - 1][x] = 0;
-              counter += 1;
+              counter = addOne(counter);
             }else if (board[i][x] === board[i - 1][x]){
               board[i][x] = board[i - 1][x] * 2;
               board[i - 1][x] = 0;
-              counter += 1; 
+              counter = addOne(counter); 
             }}}}}
   if(counter !== 0){
     addBoardNew2();
@@ -123,11 +165,11 @@ function rightBlock(){
             if (board[y][i] === 0){
               board[y][i] = board[y][i - 1];
               board[y][i - 1] = 0;
-              counter += 1;
+              counter = addOne(counter);
             }else if (board[y][i] === board[y][i - 1]){
               board[y][i] = board[y][i - 1] * 2;
               board[y][i - 1] = 0;
-              counter += 1; 
+              counter = addOne(counter); 
             }}}}}
   if(counter !== 0){
     addBoardNew2();
@@ -143,11 +185,11 @@ function leftBlock(){
             if (board[y][i] === 0){
               board[y][i] = board[y][i + 1];
               board[y][i + 1] = 0;
-              counter += 1;
+              counter = addOne(counter);
             }else if (board[y][i] === board[y][i + 1]){
               board[y][i] = board[y][i + 1] * 2;
               board[y][i + 1] = 0;
-              counter += 1; 
+              counter = addOne(counter); 
             }}}}}
   if(counter !== 0){
     addBoardNew2();
